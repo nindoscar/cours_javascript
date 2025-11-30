d7612ace3cc7a0674d16f19595bb483a938302d2
nindoscar <oscar.nindebahi@epitech.eu>
2025-11-30 23:58:52 +0000

    [ADD] Objet detaillé

---
+++ 4.Les-objets-dans-le-détail/1.Valeurs-primitives-vs-référence/script.js
@@ -0,0 +1,33 @@
+/* 
+    1. Valeurs primitives
+
+    Les valeurs primitives sont les : string, number, bigint, boolean, undefined, symbol, null.
+    Elles représentent des valeurs "simples" dans la mémoire : "abc", 15, true, etc...
+
+*/
+
+
+
+
+
+
+/* 
+    2. Les valeurs de référence.
+
+    Les objets sont des valeurs de référence, c'est-à-dire qu'ils vont prendre une place spéciale dans la mémoire.
+    Si on copie un objet dans une autre variable, nous créons une référence, nous ne créons pas une copie indépendante.
+*/
+
+
+
+
+
+
+/* 
+    3. Pourquoi les chaînes ont des propriétés / méthodes ?
+
+    Les valeurs primitives string, number, boolean et symbol sont transformées en objet lorsqu'on utilise l'opérateur "." qui sert à accèder à des propriétés.
+
+    Cela permet d'avoir accès à des méthodes et des propriétés très pratiques.
+*/
+
