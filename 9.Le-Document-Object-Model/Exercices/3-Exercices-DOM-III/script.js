32995d0714bf6ed593ca5ea318432fcb3bf8d601
nindoscar <oscar.nindebahi@epitech.eu>
2026-02-18 19:53:29 +0000

    trois methode

--- 9.Le-Document-Object-Model/Exercices/3-Exercices-DOM-III/script.js
+++ 9.Le-Document-Object-Model/Exercices/3-Exercices-DOM-III/script.js
@@ -10,7 +10,19 @@ function pushToTop(){
     //methode 1 
 
  //   document.documentElement.scrollTop = 0
-}
+
+   // methode 2
+/*
+   window.scroll({
+    top: 0,
+    behavior : "smooth"
+   })
+
+   */
+  document.documentElement.scrollIntoView({
+    behavior: "smooth", block:"start"
+  })
+  }
 /*
     2. Utilisez la méthode .scrollTo() pour scroller jusqu'au milieu de la section newsletter lorsqu'on clique sur le bouton .scroll-to-newsletter.
 
