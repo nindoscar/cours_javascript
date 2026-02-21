b6914f6f9bd5699d8236647edc1fbe5d4a5b80c2
nindoscar <oscar.nindebahi@epitech.eu>
2026-02-21 00:42:09 +0000

    formulaire

--- 10.Les-évènements/Exercices/1-Exercice-évènements-I/script.js
+++ 10.Les-évènements/Exercices/1-Exercice-évènements-I/script.js
@@ -1,3 +1,16 @@
 /*
     Prévenez le comportement par défaut du formulaire afin de récupérer les données écrites dans les inputs et les afficher dans les paragraphes correspondants.
 */
+
+const form = document.querySelector("form")
+const inputs = document.querySelectorAll("form input")
+const firstname = document.querySelector(".firstname")
+const lastname = document.querySelector(".lastname")
+
+form.addEventListener("submit", handleSubmit)
+
+function handleSubmit(e){
+   // e.preventDefault()
+    firstname.textContent = `Votre nom : ${inputs[0].value} `
+    lastname.textContent = `Votre prénoms: ${inputs[1].value} `
+}
